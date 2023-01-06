63de55b716061c2ddbfe7f3639b992d874355421
Artem Terenin <artemterenin@gmail.com>
2023-01-07 00:51:03 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,25 @@
 import genDiff from '../src/index.js';
 
-let simpleJson = '{\n  - follow: false\n    host: hexlet.io\n';
-simpleJson += '  - proxy: 123.234.53.22\n  - timeout: 50\n';
-simpleJson += '  + timeout: 20\n  + verbose: true\n}';
-const pathFirstFile = '__fixtures__/file1.json';
-const pathSecondFile = '__fixtures__/file2.json';
-test('genGendiff', () => {
-  expect(genDiff(pathFirstFile, pathSecondFile)).toEqual(simpleJson);
+let simpleMatch = '{\n  - follow: false\n    host: hexlet.io\n';
+simpleMatch += '  - proxy: 123.234.53.22\n  - timeout: 50\n';
+simpleMatch += '  + timeout: 20\n  + verbose: true\n}';
+const firstFilePath = '__fixtures__/file1.json';
+const secondFilePath = '__fixtures__/file2.json';
+const thirdFilePath = '__fixtures__/file3.yml';
+const fourthFilePath = '__fixtures__/file4.yaml';
+
+test('genGendiff simple JSON', () => {
+  expect(genDiff(firstFilePath, secondFilePath)).toEqual(simpleMatch);
+});
+
+test('genGendiff simple YML', () => {
+  expect(genDiff(thirdFilePath, fourthFilePath)).toEqual(simpleMatch);
+});
+
+test('genGendiff simple JSON/YML', () => {
+  expect(genDiff(firstFilePath, fourthFilePath)).toEqual(simpleMatch);
+});
+
+test('genGendiff simple YML/JSON', () => {
+  expect(genDiff(thirdFilePath, secondFilePath)).toEqual(simpleMatch);
 });
