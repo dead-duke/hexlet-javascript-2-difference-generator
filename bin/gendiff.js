e0ff5d12f1bbd239b8033ee9800f35504327900f
Artem Terenin <artemterenin@gmail.com>
2023-01-08 17:47:52 +0300

    add default format argument

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,7 +10,7 @@ program
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.2.0')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format', 'stylish');
 
 program.action((filepath1, filepath2) => {
   const options = program.opts();
