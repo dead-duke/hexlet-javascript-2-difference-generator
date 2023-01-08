364909caaabefffbf8c6726325b8db6c44a744c2
Artem Terenin <artemterenin@gmail.com>
2023-01-08 20:43:42 +0300

    refactoring

--- src/compareData.js
+++ src/compareData.js
@@ -2,9 +2,7 @@ import _ from 'lodash';
 
 const compareData = (data) => {
   const iter = (obj1, obj2) => {
-    const keys1 = Object.keys(obj1);
-    const keys2 = Object.keys(obj2);
-    const allKeys = _.sortBy(_.uniq([...keys1, ...keys2]));
+    const allKeys = _.sortBy(_.uniq([...Object.keys(obj1), ...Object.keys(obj2)]));
 
     return allKeys.flatMap((key) => {
       let value1 = obj1[key];
@@ -15,12 +13,8 @@ const compareData = (data) => {
         return [{ key, content: iter(value1, value2), type: 'nested' }];
       }
 
-      if (_.isObject(value1)) {
-        value1 = iter(value1, value1);
-      }
-      if (_.isObject(value2)) {
-        value2 = iter(value2, value2);
-      }
+      value1 = _.isObject(value1) ? iter(value1, value1) : value1;
+      value2 = _.isObject(value2) ? iter(value2, value2) : value2;
 
       if (!_.has(obj1, key)) {
         result.push({ key, content: value2, type: 'added' });
