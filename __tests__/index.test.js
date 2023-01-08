11d5c151b038e8e67bc361da3b56bccce1839113
Artem Terenin <artemterenin@gmail.com>
2023-01-08 17:46:44 +0300

    add errors tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,25 +1,85 @@
 import genDiff from '../src/index.js';
 
-let simpleMatch = '{\n  - follow: false\n    host: hexlet.io\n';
-simpleMatch += '  - proxy: 123.234.53.22\n  - timeout: 50\n';
-simpleMatch += '  + timeout: 20\n  + verbose: true\n}';
-const firstFilePath = '__fixtures__/file1.json';
-const secondFilePath = '__fixtures__/file2.json';
-const thirdFilePath = '__fixtures__/file3.yml';
-const fourthFilePath = '__fixtures__/file4.yaml';
+const nestMatch = `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`;
 
-test('genGendiff simple JSON', () => {
-  expect(genDiff(firstFilePath, secondFilePath)).toEqual(simpleMatch);
+const path1 = '__fixtures__/file1.json';
+const path2 = '__fixtures__/file2.json';
+const path3 = '__fixtures__/file3.yml';
+const path4 = '__fixtures__/file4.yaml';
+
+test('genDiff nest JSON', () => {
+  expect(genDiff(path1, path2, 'stylish')).toEqual(nestMatch);
 });
 
-test('genGendiff simple YML', () => {
-  expect(genDiff(thirdFilePath, fourthFilePath)).toEqual(simpleMatch);
+test('genDiff nest YML', () => {
+  expect(genDiff(path3, path4, 'stylish')).toEqual(nestMatch);
 });
 
-test('genGendiff simple JSON/YML', () => {
-  expect(genDiff(firstFilePath, fourthFilePath)).toEqual(simpleMatch);
+test('genDiff nest JSON/YML', () => {
+  expect(genDiff(path1, path4, 'stylish')).toEqual(nestMatch);
 });
 
-test('genGendiff simple YML/JSON', () => {
-  expect(genDiff(thirdFilePath, secondFilePath)).toEqual(simpleMatch);
+test('genDiff nest YML/JSON', () => {
+  expect(genDiff(path3, path2, 'stylish')).toEqual(nestMatch);
+});
+
+describe('errors', () => {
+  test('output format', () => {
+    expect(genDiff(path3, path2, 'bad output format')).toEqual(
+      'Неподдерживаемый формат вывода',
+    );
+  });
+  test('input format', () => {
+    expect(genDiff('__fixtures__/file100.js', path2, 'stylish')).toEqual(
+      'Неподдерживаемый формат файлов',
+    );
+  });
+  test('invalid path', () => {
+    expect(genDiff('__fixtures__/file10000.js', path2, 'stylish')).toEqual(
+      'По указаномму пути нет файла или директории',
+    );
+  });
 });
