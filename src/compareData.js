470b939909e7a8f9849dbc0222499781a3f59b17
Artem Terenin <artemterenin@gmail.com>
2023-01-11 01:19:02 +0300

    refactoring

--- src/compareData.js
+++ src/compareData.js
@@ -1,9 +1,22 @@
 import _ from 'lodash';
 
-const compareData = (obj1, obj2) => {
-  const allKeys = _.sortBy(_.uniq([...Object.keys(obj1), ...Object.keys(obj2)]));
-  const objectCheck = (data) => (_.isObject(data) ? compareData(data, data) : data);
+const getType = (obj1, obj2, key) => {
+  if (!_.has(obj1, key)) {
+    return 'added';
+  }
+  if (!_.has(obj2, key)) {
+    return 'removed';
+  }
+  if (obj1[key] === obj2[key]) {
+    return 'unchanged';
+  }
+  return 'updated';
+};
 
+const objectCheck = (data, f) => (_.isObject(data) ? f(data, data) : data);
+
+const compareData = (obj1, obj2) => {
+  const allKeys = _.sortBy(_.uniq(_.union(Object.keys(obj1), Object.keys(obj2))));
   return allKeys.map((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
@@ -11,21 +24,21 @@ const compareData = (obj1, obj2) => {
       return { type: 'nested', key, content: compareData(value1, value2) };
     }
 
-    if (!_.has(obj1, key)) {
-      return { type: 'added', key, content: objectCheck(value2) };
+    const type = getType(obj1, obj2, key);
+    switch (type) {
+      case 'added':
+      case 'unchanged':
+        return { type, key, content: objectCheck(value2, compareData) };
+      case 'removed':
+        return { type, key, content: objectCheck(value1, compareData) };
+      default:
+        return {
+          type,
+          key,
+          content: objectCheck(value2, compareData),
+          prevContent: objectCheck(value1, compareData),
+        };
     }
-    if (!_.has(obj2, key)) {
-      return { type: 'removed', key, content: objectCheck(value1) };
-    }
-    if (value1 === value2) {
-      return { type: 'unchanged', key, content: objectCheck(value1) };
-    }
-    return {
-      type: 'updated',
-      key,
-      content: objectCheck(value2),
-      prevContent: objectCheck(value1),
-    };
   });
 };
 
