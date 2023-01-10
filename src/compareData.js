1c1b203cfc5657341ac114f85d427035e47b0c4b
Artem Terenin <artemterenin@gmail.com>
2023-01-11 00:36:13 +0300

    refactoring

--- src/compareData.js
+++ src/compareData.js
@@ -1,42 +1,32 @@
 import _ from 'lodash';
 
-const compareData = (data) => {
-  const iter = (obj1, obj2) => {
-    const allKeys = _.sortBy(_.uniq([...Object.keys(obj1), ...Object.keys(obj2)]));
+const compareData = (obj1, obj2) => {
+  const allKeys = _.sortBy(_.uniq([...Object.keys(obj1), ...Object.keys(obj2)]));
+  const objectCheck = (data) => (_.isObject(data) ? compareData(data, data) : data);
 
-    return allKeys.flatMap((key) => {
-      let value1 = obj1[key];
-      let value2 = obj2[key];
-      const result = [];
+  return allKeys.map((key) => {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+    if (_.isObject(value1) && _.isObject(value2)) {
+      return { type: 'nested', key, content: compareData(value1, value2) };
+    }
 
-      if (_.isObject(value1) && _.isObject(value2)) {
-        return [{ key, content: iter(value1, value2), type: 'nested' }];
-      }
-
-      value1 = _.isObject(value1) ? iter(value1, value1) : value1;
-      value2 = _.isObject(value2) ? iter(value2, value2) : value2;
-
-      if (!_.has(obj1, key)) {
-        result.push({ key, content: value2, type: 'added' });
-      } else if (!_.has(obj2, key)) {
-        result.push({ key, content: value1, type: 'removed' });
-      } else if (value1 === value2) {
-        result.push({ key, content: value1, type: 'unchanged' });
-      } else {
-        result.push({ key, content: value1, type: 'updated/removed' });
-        result.push({
-          key,
-          content: value2,
-          prevContent: value1,
-          type: 'updated',
-        });
-      }
-
-      return result;
-    });
-  };
-
-  return iter(...data);
+    if (!_.has(obj1, key)) {
+      return { type: 'added', key, content: objectCheck(value2) };
+    }
+    if (!_.has(obj2, key)) {
+      return { type: 'removed', key, content: objectCheck(value1) };
+    }
+    if (value1 === value2) {
+      return { type: 'unchanged', key, content: objectCheck(value1) };
+    }
+    return {
+      type: 'updated',
+      key,
+      content: objectCheck(value2),
+      prevContent: objectCheck(value1),
+    };
+  });
 };
 
 export default compareData;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import formatter from './formatters/index.js';
 
 const genDiff = (path1, path2, outputFormat) => {
   try {
-    const data = parseData(path1, path2);
-    const comparedData = compareData(data);
+    const [data1, data2] = parseData(path1, path2);
+    const comparedData = compareData(data1, data2);
     return formatter(comparedData, outputFormat);
   } catch (err) {
     return err.message;
