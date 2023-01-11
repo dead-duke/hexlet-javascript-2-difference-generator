f7e50f8a75cb910f0bb386faac970bf6ad401799
Artem Terenin <artemterenin@gmail.com>
2023-01-11 13:21:04 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import parseData from './parseData.js';
 import compareData from './compareData.js';
 import formatter from './formatters/index.js';
 
-const genDiff = (path1, path2, outputFormat) => {
+const genDiff = (path1, path2, outputFormat = 'stylish') => {
   try {
     const [data1, data2] = parseData(path1, path2);
     const comparedData = compareData(data1, data2);
