f0bd085d853b339c078d1d064b1355ff79021f74
Artem Terenin <artemterenin@gmail.com>
2023-01-12 11:36:14 +0300

    change error messages

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -35,17 +35,12 @@ const filePath2 = path.resolve('__fixtures__/testFile2.json');
 describe('errors', () => {
   test('output format', () => {
     expect(genDiff(filePath1, filePath2, 'bad output format')).toEqual(
-      'Неподдерживаемый формат вывода',
+      'Unsupported output format',
     );
   });
   test('input format', () => {
     expect(genDiff('__fixtures__/extendTestFile.js', filePath1, 'stylish')).toEqual(
-      'Неподдерживаемый формат файлов',
-    );
-  });
-  test('invalid path', () => {
-    expect(genDiff('__fixtures__/ghost.js', filePath1, 'stylish')).toEqual(
-      'По указаномму пути нет файла или директории',
+      'Unsupported file format',
     );
   });
 });
