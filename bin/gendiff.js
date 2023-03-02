1823a41f39cf4cb431431671557c737e8d4cc954
Artem Terenin <artemterenin@gmail.com>
2023-03-02 16:42:55 +0300

    refactoring

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,9 +10,7 @@ program
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.2.0')
-  .option('-f, --format [type]', 'output format', 'stylish');
-
-program
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
     const options = program.opts();
     console.log(genDiff(filepath1, filepath2, options.format));
