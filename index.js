1198fa96d6edc76f31d940e4e4db9caf0818fa58
Artem Terenin <artemterenin@gmail.com>
2023-03-02 16:18:14 +0300

    change index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-import getParseData from './src/getParseData.js';
-import getComparedData from './src/getComparedData.js';
-import getFormattedData from './src/formatters/getFormattedData.js';
-
-const genDiff = (path1, path2, outputFormat = 'stylish') => {
-  try {
-    const data1 = getParseData(path1);
-    const data2 = getParseData(path2);
-    const comparedData = getComparedData(data1, data2);
-    return getFormattedData(comparedData, outputFormat);
-  } catch (err) {
-    return err.message;
-  }
-};
+import genDiff from './src/index.js';
 
 export default genDiff;
